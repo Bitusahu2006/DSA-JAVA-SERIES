eb0d85703abbba66cfaf199d6c714ab2b26f7752
rsrishti355-eng <rsrishti355@gmail.com>
2026-08-11 14:51:24 +0530

    Add on Github

---
+++ Array/Multi2Darray.java
@@ -0,0 +1,15 @@
+public class Multi2Darray {
+    public static void main(String args[]){
+
+        int ans=1;
+        int arr[][]={{1,2,3},{1,2,3}};
+
+        for(int i=0; i<arr.length; i++){
+            for(int j=1;j<arr[i].length; j++){
+                int value=arr[i][j];
+                ans*=value;
+            }
+        }
+        System.out.print(ans);
+    }
+}
